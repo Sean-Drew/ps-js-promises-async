d57606a46b404d218b11a4f94a63289e77dd6320
Sean-Drew <SDrew989@gmail.com>
2021-02-10 13:20:52 -0500

    Chained promise
    Added a promise chain

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -4,7 +4,9 @@ export function get() {
     axios.get('http://localhost:3000/orders/1')
     .then(({ data }) => {
         setText(JSON.stringify(data))
+        return 'Pluralsight'
     })
+    .then(res => console.log(res))
 }
 
 export function getCatch() {
@@ -16,6 +18,15 @@ export function getCatch() {
 }
 
 export function chain() {
+    axios.get('http://localhost:3000/orders/1')
+    .then(({ data }) => {
+        // if no return is specified in the promise chain, js default behavior returns undefined
+        // keep the promise chain going by specifying a return value
+        return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`)
+    })
+    .then(({ data }) => {
+        setText(`City: ${data.city}`)
+    })
 }
 
 export function chainCatch() {
