dc9da8a756aaab67c7b1d623b511e35997f79338
Sean-Drew <SDrew989@gmail.com>
2021-02-10 16:42:08 -0500

    Async/await get call

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -1,6 +1,9 @@
 import setText , {appendText} from './results.mjs';
 
-export function get(){
+export async function get(){
+    // can only use await inside an async function
+    const { data } = await axios.get('http://localhost:3000/orders/1')
+    setText(JSON.stringify(data))
 }
 
 export function getCatch(){
