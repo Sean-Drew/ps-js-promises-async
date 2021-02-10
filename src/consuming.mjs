d955a674f603a3b531d753d21d3d994dbc40a80e
Sean-Drew <SDrew989@gmail.com>
2021-02-10 13:36:45 -0500

    Finally
    Added .finally statement to the end of a promise chain to run code after
     the promises are resolved, independent of whether the promises were
     fulfilled or rejected.

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -43,4 +43,22 @@ export function chainCatch() {
 }
 
 export function final() {
+    showWaiting()
+    axios.get('http://localhost:3000/orders/1')
+    .then(({ data }) => {
+        return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`)
+    })
+    .then(({ data }) => {
+        setText(`City: ${data.city}`)
+    })
+    .catch(err => setText(err))
+    .finally(() => {
+        // for demo, setting a timeout here so the waiting screen is shown, then removed after promise success
+        setTimeout(() => {
+            hideWaiting()
+        }, 1500)
+
+        // for demo, appending some text to the results box to show this code ran to the end
+        appendText(' -- Completely Done')
+    })
 }
