600354a491488403f979cec4cbdc924ed2b718df
Sean-Drew <SDrew989@gmail.com>
2021-02-10 17:06:02 -0500

    Awaiting concurrent axios requests

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -24,7 +24,25 @@ export async function chain(){
     setText(`City: ${JSON.stringify(address.city)}`)
 }
 
-export function concurrent(){
+export async function concurrent(){
+    // awaiting concurrent requests.
+
+    // no await here. not putting an await on the axios calls here allows the calls to initiate at 
+    // the same time.
+    const orderStatus = axios.get('http://localhost:3000/orderStatuses')
+    const orders = axios.get('http://localhost:3000/orders')
+
+    setText('')
+
+    // adding the await down here lets the calls be used in an order that makes sense, especially if
+    // one is dependent on the other.
+    // This says we don't want to do anything with orders until we get the statuses first (statuses in this
+    // case is slower).
+    const { data: statuses } = await orderStatus
+    const { data: order } = await orders
+
+    appendText(JSON.stringify(statuses))
+    appendText(JSON.stringify(order[0]))
 }
 
 export function parallel(){
