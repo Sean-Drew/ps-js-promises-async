7d88237cc53682210f7556e802548fcd039b2a9b
Sean-Drew <SDrew989@gmail.com>
2021-02-10 13:29:55 -0500

    Catch error in chain
    Added catch to a promise chain to handle errors.

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -10,7 +10,7 @@ export function get() {
 }
 
 export function getCatch() {
-        axios.get('http://localhost:3000/orders/123')
+    axios.get('http://localhost:3000/orders/123')
     .then(({ data }) => {
         setText(JSON.stringify(data))
     })
@@ -20,16 +20,26 @@ export function getCatch() {
 export function chain() {
     axios.get('http://localhost:3000/orders/1')
     .then(({ data }) => {
+        // console.log('data from order axios call: ', data)
         // if no return is specified in the promise chain, js default behavior returns undefined
         // keep the promise chain going by specifying a return value
         return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`)
     })
     .then(({ data }) => {
         setText(`City: ${data.city}`)
+        // console.log('data from address axios call: ', data)
     })
 }
 
 export function chainCatch() {
+    axios.get('http://localhost:3000/orders/1')
+    .then(({ data }) => {
+        return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`)
+    })
+    .then(({ data }) => {
+        setText(`City: ${data.my.city}`)
+    })
+    .catch(err => setText(err))
 }
 
 export function final() {
