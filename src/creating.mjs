77731015b9990476bc56baad5d7c922f10cbb324
Sean-Drew <SDrew989@gmail.com>
2021-02-10 14:43:34 -0500

    Promise.all
    Fired multiple axios requests, handled with Promise.all() which will
    wait until all promises are resolved, or stop if 1 promise is rejected.

--- src/creating.mjs
+++ src/creating.mjs
@@ -41,9 +41,46 @@ export function clearIntervalChain(){
 }
 
 export function xhr(){
+    let request = new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest()
+        // for demo, user 7 does not exist, will throw error 404, xhr status text will be 'not found'
+        xhr.open('Get', 'http://localhost:3000/users/7')
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                resolve(xhr.responseText)
+            } else {
+                reject(xhr.statusText)
+            }
+        }
+        xhr.onerror = () => reject('Request Failed')
+        xhr.send()
+    })
+
+    request.then(result => setText(result))
+        .catch(reason => setText(reason))
 }
 
 export function allPromises(){
+    // fire multiple get requests
+    let categories = axios.get('http://localhost:3000/itemCategories')
+    let statuses = axios.get('http://localhost:3000/orderStatuses')
+    let userTypes = axios.get('http://localhost:3000/userTypes')
+    let addressTypes = axios.get('http://localhost:3000/addressTypes')
+
+    // we want all of them to be fulfilled before moving on, so we use promise.all and pass them as a params array
+    // the all method waits until all promises are fulfilled, or if one promise is rejected. all or none.
+    Promise.all([categories, statuses, userTypes, addressTypes])
+        // array of results, in the order they were added
+        .then(([cat, stat, type, address]) => {
+            setText('')
+
+            appendText(JSON.stringify(cat.data))
+            appendText(JSON.stringify(stat.data))
+            appendText(JSON.stringify(type.data))
+        })
+        .catch(reasons => {
+            setText(reasons)
+        })
 }
 
 export function allSettled(){
